ab08af75e580b8d70c0a5578b9fc2fac0dfd659c
Jordan Lipson <lipsonjordan@gmail.com>
2023-05-06 11:47:18 -0400

    Completed styling for Select Level page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,20 +1,83 @@
 import React from 'react';
 import logo from '../images/Logo.svg';
+import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+
+const LandingContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    height: 100%;
+    padding-top: 40vh;
+`;
+
+const LogoImage = styled.img`
+    width: 70%;
+    max-width: 400px;
+    margin-left: 40px;
+    height: auto;
+`;
+
+const TextContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 290px;
+    margin-left: 40px;
+`;
+
+const Subheading = styled.p`
+    color: #fff;
+    font-size: 18px;
+    // font-weight: 400;
+    margin-bottom: 40px;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-left: 40px;
+    gap: 16px;
+`;
+
+const SignInButton = styled.button`
+    width: 150px;
+    height: 40px;
+    border: none;
+    border-radius: 38px;
+    background: white;
+    color: #394B60;
+    font-size: 15px;
+`;
+
+const SignUpButton = styled.button`
+    width: 150px;
+    height: 40px;
+    border: 2px solid white;
+    border-radius: 38px;
+    background: transparent;
+    color: white;
+    font-size: 15px;
+`;
 
 const Landing = () => {
     const navigate = useNavigate();
 
     return (
         <div>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100vh', background: 'linear-gradient(to bottom, #256193, #296093, #564889)' }}>
-                <img src={logo} alt="Logo" style={{ height: '200px', marginBottom: '50px' }} />
-                <p style={{ fontSize: '24px', fontWeight: 'bold', color: 'white', textAlign: 'center', maxWidth: '800px', margin: '0 50px 50px' }}>Experience a new dimension in language learning with our AR-powered conversation simulation.</p>
-                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <button style={{ background: 'white', color: '#256193', borderRadius: '20px', padding: '10px 20px', fontSize: '18px', marginRight: '20px' }} onClick={() => navigate("/signup")}>Sign Up</button>
-                    <button style={{ background: 'white', color: '#256193', borderRadius: '20px', padding: '10px 20px', fontSize: '18px' }} onClick={() => navigate("/signin")}>Sign In</button>
-                </div>
-            </div>
+            <LandingContainer>
+                <LogoImage src={logo} alt="speakAR Logo" />
+                <TextContainer>
+                    <Subheading>
+                        Experience a new dimension in language learning with our AR-powered conversation simulation.
+                    </Subheading>
+                </TextContainer>
+                <ButtonContainer>
+                    <SignInButton onClick={() => navigate("/signin")}>SIGN IN</SignInButton>
+                    <SignUpButton onClick={() => navigate("/signup")}>SIGN UP</SignUpButton>
+                </ButtonContainer>
+            </LandingContainer>
         </div>
     );
 }
