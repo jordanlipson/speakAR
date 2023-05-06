4a69b73a8ebcee9a10735f7aca795fbcbdd818e9
ishan-singh-3005 <ishan423@gmail.com>
2023-05-06 11:40:13 -0400

    added text to speech

--- frontend/src/pages/ArPage.js
+++ frontend/src/pages/ArPage.js
@@ -14,6 +14,12 @@ const ArPage = () => {
     return <span>Browser doesn't support speech recognition.</span>;
     }
 
+    const speak = () => {
+        const speech = new SpeechSynthesisUtterance();
+        speech.text = 'Hello, this is a test.';
+        window.speechSynthesis.speak(speech);
+      };
+
     return (
         <div>
             <iframe src="https://api.echo3d.co/webar?key=hidden-voice-5514&entry=4465f250-2398-4019-a5fb-c18b701fcde3" width="100%" height="500" title="echoAR WebAR iframe element"></iframe>
@@ -23,6 +29,7 @@ const ArPage = () => {
                 <button onClick={SpeechRecognition.stopListening}>Stop</button>
                 <button onClick={resetTranscript}>Reset</button>
                 <p>{transcript}</p>
+                <button onClick={speak}>SPEAK</button>
             </div>
         </div>
     );
