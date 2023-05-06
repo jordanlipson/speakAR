7e569485a403f27c3223f255dc2e9c299ee09bac
ishan-singh-3005 <ishan423@gmail.com>
2023-05-05 21:09:00 -0400

    connection to database

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,19 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from flask import Flask
 
 app = Flask(__name__)
+
+# CHANGE THE USER AND PASS
+uri = "mongodb+srv://<user>:<pass>@cluster0.hpi1zla.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 @app.route('/')
 def index():
