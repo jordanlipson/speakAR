1e7542440631f825b9e3edade53768abe5c04615
Jordan Lipson <lipsonjordan@gmail.com>
2023-05-05 22:10:09 -0400

    Reorganized file system, added component and image folders, added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, HashRouter, Route, Routes, Link } from "react-router-dom";
+import "./components/App.css"
+import { Landing } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <iframe src="https://api.echo3d.co/webar?key=hidden-voice-5514&entry=4465f250-2398-4019-a5fb-c18b701fcde3" width="100%" height="500" title="echoAR WebAR iframe element"></iframe>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </Router>
+      {/* <iframe src="https://api.echo3d.co/webar?key=hidden-voice-5514&entry=4465f250-2398-4019-a5fb-c18b701fcde3" width="100%" height="500" title="echoAR WebAR iframe element"></iframe> */}
     </div>
   );
 }
---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <div>
+            <div className='landing' id='landing'>
+                <h1>Welcome to speakAR</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Landing
