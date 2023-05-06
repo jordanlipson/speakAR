e169a1a7fccfdbbdde9f8a491751db3209d6b4a3
ishan-singh-3005 <ishan423@gmail.com>
2023-05-05 20:30:51 -0400

    added a ar of a fox

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe src="https://api.echo3d.co/webar?key=hidden-voice-5514&entry=4465f250-2398-4019-a5fb-c18b701fcde3" width="100%" height="500" title="echoAR WebAR iframe element"></iframe>
+    </div>
+  );
+}
+
+export default App;
