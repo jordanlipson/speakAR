36c46068a046b9a494ce8627069b58313aaf0dd7
ishan-singh-3005 <ishan423@gmail.com>
2023-05-06 02:22:15 -0400

    login working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, HashRouter, Route, Routes, Link } from "react-router-dom";
-import { Landing } from "./components";
+import { Landing, SignUp, SignIn } from "./components";
 // import "./components/App.css"
 import GlobalStyle from "./globalStyles";
-import SignUp from "./components/SignUp";
 
 function App() {
   return (
@@ -13,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
         </Routes>
       </Router>
       {/* <iframe src="https://api.echo3d.co/webar?key=hidden-voice-5514&entry=4465f250-2398-4019-a5fb-c18b701fcde3" width="100%" height="500" title="echoAR WebAR iframe element"></iframe> */}
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,1 +1,5 @@
 export { default as Landing } from './Landing';
+export { default as SignUp } from './SignUp';
+export { default as SignIn } from './SignIn';
+
+
