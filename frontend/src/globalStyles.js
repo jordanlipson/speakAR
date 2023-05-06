80673672729e6d08e26fb20f8c5d57b2669d498e
Jordan Lipson <lipsonjordan@gmail.com>
2023-05-06 03:26:36 -0400

    Added Google Font: Outfit to global styles

--- frontend/src/globalStyles.js
+++ frontend/src/globalStyles.js
@@ -1,11 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    background: linear-gradient(to bottom, #256193, #296093, #564889);
-    height: 100vh;
-    // width: 100vw;
-  }
+    @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap');
+
+    body {
+        background: linear-gradient(to bottom, #256193, #296093, #564889);
+        height: 100vh;
+        // width: 100vw;
+    }
 `;
 
 export default GlobalStyle;
